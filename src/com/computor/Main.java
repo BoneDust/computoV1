76750f9e7471c2965d9b39e05077aa9f3f5c906a
gtshekel <gtshekel@student.42.fr>
2018-10-15 18:34:58 +0200

    need to fix leading sign handler

--- src/com/computor/Main.java
+++ src/com/computor/Main.java
@@ -1,10 +1,8 @@
 package com.computor;
 
-import org.omg.CORBA.SystemException;
 
-import javax.print.DocFlavor;
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main
@@ -33,32 +31,36 @@ public class Main
 
     }
 
-    static void solveForX(int a, int b, int c)
+    static void solveForX(double a, double b, double c)
     {
 
         ArrayList<Double> answers = new ArrayList<>();
         if (a == 0 && b == 0 && c != 0)
         {
-            System.out.println("There exit no solutions for this polynomial");
+            System.out.println("There exit no solutions for this polynomial.");
             return;
         }
 
         if (a == 0 && b == 0 && c == 0)
         {
-            System.out.println("The set of real numbers is the solution");
+            System.out.println("The set of real numbers is the solution.");
             return;
         }
-        
+        if (a == 0)
+        {
+            System.out.println("There's one solution.\n" + (-(c / b)));
+            return;
+        }
         double discriminant = (b * b) - (4 * a * c);
         if (discriminant == 0)
         {
-            System.out.println("The disciminant is: " + discriminant + "There is one solution");
+            System.out.println("The disciminant is: " + discriminant + "\nThere is one solution.");
             double ans = (-b + sqrt(discriminant, 0.00000001)) / (2 * a);
             System.out.println(ans);
         }
         else if (discriminant > 0)
         {
-            System.out.println("The disciminant is: " + discriminant + "There are two solutions");
+            System.out.println("The disciminant is: " + discriminant + "\nThere are two solutions.");
             double ans1 = (-b + sqrt(discriminant, 0.00000001)) / (2 * a);
             double ans2 = (-b - sqrt(discriminant, 0.00000001)) / (2 * a);
             System.out.println(ans1);
@@ -66,7 +68,7 @@ public class Main
         }
         else
         {
-            System.out.println("The disciminant is: " + discriminant + "There two complex solutions");
+            System.out.println("The disciminant is: " + discriminant + "\nThere two complex solutions.");
             double firstPart = (-b / (2 * a)), secondPart = sqrt(discriminant, 0.00000001)/ (2 * a);
             String complex1 = firstPart + " + " + secondPart + "i";
             String complex2 = firstPart + " - " + secondPart + "i";
@@ -80,22 +82,29 @@ public class Main
         ArrayList<Term> terms = new ArrayList<>();
 
         String[] split = equation.split("=");
+        if (split.length != 2)
+        {
+            System.out.print("Invalid equation.");
+            System.exit(0);
+        }
         String sideString  = side.equals("LHS") ? split[0] : split[1];
         ArrayList<Integer> signs = getSigns(sideString);
         String[] tempTerms = sideString.split("\\+|-");
         boolean firstTermSign = tempTerms.length == signs.size();
         if (tempTerms.length < signs.size())
         {
-            System.out.println("To many Signs detected");
+            System.out.println("Cant solve equation. Too many Signs detected");
             System.exit(0);
         }
         for (int index = 0; index < tempTerms.length; index++)
         {
+            if (index == 0 && tempTerms[index].equals(" "))
+                continue;
             if (isValidTerm(tempTerms[index]))
             {
                 String[] factors = tempTerms[index].split("\\*");
                 double coefficient = Double.parseDouble(factors[0].trim());
-                int power = Integer.parseInt(factors[1].trim().split("^")[1]);//todo not sure about ^
+                int power = Integer.parseInt(factors[1].trim().split("\\^")[1].trim());
                 int sign;
                 if (index == 0)
                 {
@@ -120,10 +129,9 @@ public class Main
     private static boolean isValidTerm(String termString)
     {
         String[] factors = termString.split("\\*");
-        if (factors.length != 2 || !factors[0].trim().matches("^\\d+(\\.\\d+)?$") ||
-           !factors[1].trim().matches("X^\\d+$"))//todo not so sure here
+        if (factors.length != 2 || !factors[0].trim().matches("\\d+(\\.\\d+)?") || !factors[1].trim().matches("X\\s?\\^\\s?[012]"))
         {
-            System.out.println("Invalid term detected: " + termString);
+            System.out.println("Can't solve equation. Invalid term detected: " + termString);
             System.exit(0);
         }
         return (true);
@@ -144,22 +152,90 @@ public class Main
 
     private static ArrayList<Term> getReducedForm( ArrayList<Term> lhsTerms, ArrayList<Term> rhsTerms)
     {
-        ArrayList<Term> reducedForm = new ArrayList<>()
+        ArrayList<Term> reducedForm = new ArrayList<>();
+        for (int power = 0; power < 3; power++)
         {
 
+            double new_coefficient = 0;
+            for (Term term : lhsTerms)
+            {
+                if (term.getPower() == power)
+                {
+                    new_coefficient += term.getSign() * term.getCo_efficient();
+                }
 
+            }
+            for (Term term : rhsTerms)
+            {
+                if (term.getPower() == power)
+                {
+                    new_coefficient += -1 * (term.getSign() * term.getCo_efficient());
+                }
+            }
+
+            if (new_coefficient != 0)
+            {
+                reducedForm.add(new Term(dabs(new_coefficient), power, new_coefficient > 0? 1 : -1));
+            }
         }
+        return (reducedForm);
+    }
+
+    private static  void printReducedForm(ArrayList<Term> reducedForm)
+    {
+        System.out.print("Reduced form: ");
+        for (int i = 0; i< reducedForm.size(); i++)
+        {
+            if (i == 0)
+            {
+                if (reducedForm.get(i).getSign()  < 0)
+                    System.out.print("-" + reducedForm.get(i).getCo_efficient() + " * X^" + reducedForm.get(i).getPower() + " ");
+                else
+                    System.out.print(reducedForm.get(i).getCo_efficient() + " * X^" + reducedForm.get(i).getPower() + " ");
+            }
+            else
+            {
+                if (reducedForm.get(i).getSign()  < 0)
+                    System.out.print("- " + reducedForm.get(i).getCo_efficient() + " * X^" + reducedForm.get(i).getPower() + " ");
+                else
+                    System.out.print("+ " + reducedForm.get(i).getCo_efficient() + " * X^" + reducedForm.get(i).getPower() + " ");
+            }
+        }
+        System.out.println("= 0\nPolynomial degree: " + (reducedForm.size() > 0 ? reducedForm.size() - 1 : "0"));
+    }
+
+    private static  Map<String, Double> getABC(ArrayList<Term> reducedForm)
+    {
+        Map<String, Double> abc = new HashMap<>();
+
+        for(Term term : reducedForm)
+        {
+            if (term.getPower() == 0)
+                abc.put("c", term.getSign() * term.getCo_efficient());
+            else if (term.getPower() == 1)
+                abc.put("b", term.getSign() * term.getCo_efficient());
+            else //if (term.getPower() == 2)
+                abc.put("a", term.getSign() * term.getCo_efficient());
+        }
+        if (!abc.containsKey("a"))
+            abc.put("a", 0.0);
+        if (!abc.containsKey("b"))
+            abc.put("b", 0.0);
+        if (!abc.containsKey("c"))
+            abc.put("c", 0.0);
+
+        return (abc);
     }
 
 
 
     public static void main(String[] args)
     {
-        ArrayList<Term> lhsTerms = getTerms(args[1], "LHS");
-        ArrayList<Term> rhsTerms = getTerms(args[1], "RHS");
+        ArrayList<Term> lhsTerms = getTerms(args[0], "LHS");
+        ArrayList<Term> rhsTerms = getTerms(args[0], "RHS");
         ArrayList<Term> reducedForm = getReducedForm(lhsTerms, rhsTerms);
         printReducedForm(reducedForm);
-        Map<String, Integer> abc = getABC(reducedForm);
+        Map<String, Double> abc = getABC(reducedForm);
         solveForX(abc.get("a"), abc.get("b"), abc.get("c"));
     }
 }
